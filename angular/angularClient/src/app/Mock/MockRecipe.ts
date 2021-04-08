4a8b1f29b9d775d09f6ae4a86338be69b6ac3d52
bepix <lavoie.felix@hotmail.com>
2021-04-07 23:16:51 -0400

    recipe list small fix

--- angular/angularClient/src/app/Mock/MockRecipe.ts
+++ angular/angularClient/src/app/Mock/MockRecipe.ts
@@ -6,5 +6,7 @@ export const RECIPES: Recipe[] = [
    new Recipe (3, "Mille-feuille", "dont do it youre gonna fuck it up anyway", ["Souls of the damned"]),
    new Recipe (4, "Pokebol", "Gotta catch em all", ["Pokeball", "1 grit sand paper"] ),
    new Recipe (5, "Shit", "Just shti", ["Shit"]),
-   new Recipe (6, "sfda", "fdsgdfhdshgrrshgfh",["benis"] )
+   new Recipe (6, "sfda", "fdsgdfhdshgrrshgfh",["benis"] ),
+   new Recipe (7, "fdsfsdfsfda", "fdsgfddfhdshgrrshgfh",["benis"] )
+
   ];
