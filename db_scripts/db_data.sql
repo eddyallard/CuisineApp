ea3e15a3e2b7324fd0ac6cb9667f5a3324b0797f
Eddy <eallard1307@gmail.com>
2021-04-15 10:58:55 -0400

    Fixed dependencies between tables

---
+++ db_scripts/db_data.sql
@@ -0,0 +1,56 @@
+INSERT INTO Ingredient(IngredientName,measureType)
+VALUES('flour', 'g.'),
+('egg','unit'),
+('milk','ml.'),
+('chicken breast','g.'),
+('garlic clove','unit'),
+('sugar','g.'),
+('salt','g.'),
+('rice','g.'),
+('vegetable oil','ml.'),
+('ham','unit'),
+('beef steak','unit'),
+('ginger','g.'),
+('cheddar cheese','g.'),
+('mozzarella cheese','g.'),
+('scallions','g.'),
+('tofu','g.'),
+('carrot','unit'),
+('brocoli','unit'),
+('honey', 'ml.'),
+('mustard', 'ml.'),
+('soy sauce','ml.'),
+('pepper','g.'),
+('orange', 'unit'),
+('whole turkey' , 'unit'),
+('white onion','unit'),
+('corn cob','unit'),
+('mayonnaise','ml.'),
+('common mushroom','unit'),
+('lemon','unit'),
+('beet', 'unit'),
+('turnip','unit'),
+('shrimp','g.'),
+('tomato','unit'),
+('chicken broth','unit'),
+('almonds','g.');
+
+INSERT INTO User(UserName, email, UserPassword)
+VALUES ("Jean Guy", "jeanguy@jmail.ca", "qwerty"),
+("Pierre Paul", "ppaul@coldmail.com", "soleil123"),
+("Arsène Lupin", "gentleman@cambrioleur.fr", "password"),
+("François Legault", "legault@aweilleamaison.qc.ca", "horacio");
+
+INSERT INTO Recipe(RecipeName, RecipeInstruction, UserId)
+VALUES ("Chat farci", "Prendre une farce et la mettre dans le chat.", 1),
+("Poulet aux saveurs", "Faire cuire le poulet dans toutes les saveurs.", 2),
+("Tarte aux pommes", "Prendre les pommes et les mettre dans la tarte.", 4);
+
+INSERT INTO RecipeIngredient(IngredientId, RecipeId, Quantity)
+VALUES (1,1,2), (4,1,60), (31,1,76), (1,2,2), (6,2,60), (27,2,76), (26,3,1300);
+
+INSERT INTO UserIngredient(IngredientId, UserId, Quantity)
+VALUES (10,4,2), (14,1,60), (3,1,76), (11,2,2), (16,2,60), (7,2,76), (6,3,1300);
+
+INSERT INTO Vote(UserId, RecipeId, VoteValue)
+VALUES (1,1,1), (2,1,1), (4,2,-1);
---
+++ db_scripts/db_schema.sql
@@ -0,0 +1,58 @@
+USE school;
+
+DROP TABLE IF EXISTS Vote;
+DROP TABLE IF EXISTS UserIngredient;
+DROP TABLE IF EXISTS RecipeIngredient;
+DROP TABLE IF EXISTS  Recipe;
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS  Ingredient;
+
+CREATE table User (
+	UserId int NOT NULL AUTO_INCREMENT primary key,
+    UserName varchar(50) NOT NULL,
+    email varchar(50) NOT NULL,
+    UserPassword varchar(50) NOT NULL
+);
+
+CREATE table Recipe (
+	RecipeId int NOT NULL AUTO_INCREMENT primary key,
+    RecipeName varchar(50),
+    RecipeInstruction varchar(1000),
+    UserId int,
+    foreign key(UserId) REFERENCES User(UserId)
+);
+
+CREATE table Ingredient (
+	IngredientId int NOT NULL AUTO_INCREMENT primary key,
+    IngredientName varchar(50),
+    measureType ENUM('g.','ml.','unit')
+);
+
+CREATE table RecipeIngredient (
+	IngredientId int NOT NULL,
+    RecipeId int NOT NULL,
+    Quantity float,
+    primary key(IngredientId, RecipeId),
+    foreign key(IngredientId) REFERENCES Ingredient(IngredientId) ON DELETE CASCADE,
+    foreign key(RecipeId) REFERENCES Recipe(RecipeId) ON DELETE CASCADE
+);
+
+CREATE table UserIngredient (
+	IngredientId int NOT NULL,
+    UserId int NOT NULL,
+    Quantity float,
+    primary key(IngredientId, UserId),
+    foreign key(IngredientId) REFERENCES Ingredient(IngredientId) ON DELETE CASCADE,
+    foreign key(UserId) REFERENCES User(UserId) ON DELETE CASCADE
+);
+
+CREATE table Vote (
+	UserId int NOT NULL,
+    RecipeId int NOT NULL,
+    VoteValue int NOT NULL default 0,
+    primary key(UserId, RecipeId),
+    foreign key(UserId) REFERENCES User(UserId) ON DELETE CASCADE,
+    foreign key(RecipeId) REFERENCES Recipe(RecipeId) ON DELETE CASCADE
+);
+
+
