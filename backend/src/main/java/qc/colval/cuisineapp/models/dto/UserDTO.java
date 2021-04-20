0c714f768d80f00716517ffb6e075100d01629e9
Eddy <eallard1307@gmail.com>
2021-04-20 11:21:17 -0400

    Modified class User and it's DTO's

--- backend/src/main/java/qc/colval/cuisineapp/models/dto/UserDTO.java
+++ backend/src/main/java/qc/colval/cuisineapp/models/dto/UserDTO.java
@@ -1,9 +1,12 @@
 package qc.colval.cuisineapp.models.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import qc.colval.cuisineapp.models.dto.user.DataDTO;
+import qc.colval.cuisineapp.models.dto.user.SupportDTO;
 
 import java.util.List;
 
@@ -12,8 +15,10 @@ import java.util.List;
 @Getter
 @Setter
 public class UserDTO {
-    private Integer userId;
-    private String userName;
-    private String email;
-    private String userPassword;
+    @JsonProperty("data")
+    private DataDTO dataDTO;
+
+    @JsonProperty("support")
+    private SupportDTO supportDTO;
+
 }
--- backend/src/main/java/qc/colval/cuisineapp/models/entities/User.java
+++ backend/src/main/java/qc/colval/cuisineapp/models/entities/User.java
@@ -6,6 +6,8 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Entity
@@ -34,4 +36,24 @@ public class User implements Serializable {
     @Size(min = 2, max = 50)
     @NotNull
     private String userPassword;
+
+    @Column(name = "Roles")
+    private String roles = "";
+
+    @Column(name = "Permissions")
+    private String permissions = "";
+
+    public List<String> getRolesList() {
+        if (this.roles.length() > 0) {
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+
+    public List<String> getPermissionsList() {
+        if (this.roles.length() > 0) {
+            return Arrays.asList(this.permissions.split(","));
+        }
+        return new ArrayList<>();
+    }
 }
