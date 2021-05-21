504f8ff47a5dbf7876756075d57138d6659009d7
Eddy <eallard1307@gmail.com>
2021-05-21 18:23:03 -0400

    CORS AGAIN

--- backend/src/main/java/qc/colval/cuisineapp/config/WebSecurityConfig.java
+++ backend/src/main/java/qc/colval/cuisineapp/config/WebSecurityConfig.java
@@ -7,10 +7,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import qc.colval.cuisineapp.repositories.IUserRepository;
 import qc.colval.cuisineapp.security.JwtAuthenticationFilter;
 import qc.colval.cuisineapp.security.JwtAuthorizationFilter;
@@ -22,6 +24,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserPrincipalDetailService userPrincipalDetailService;
     private final IUserRepository repos;
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/api/**").anyRequest();
+    }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());
