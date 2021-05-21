8948dbbb4dfa3f25664b3d88171bcb8f5eee5ba2
Eddy <eallard1307@gmail.com>
2021-05-21 19:19:43 -0400

    COCOCOOCOC

--- backend/src/main/java/qc/colval/cuisineapp/config/WebSecurityConfig.java
+++ backend/src/main/java/qc/colval/cuisineapp/config/WebSecurityConfig.java
@@ -76,9 +76,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"));
+        //configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        //configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
